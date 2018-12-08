ee7c72fe6cc23da829c34a9728211330064e4205
stevewitman <stevewitman@gmail.com>
2018-12-07 22:34:38 -0700

    Add components and routes for links and page-not-found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ToComponent } from './to/to.component';
+import { LinksComponent } from './links/links.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  { path: 'links', component: LinksComponent },
+  { path: '', component: LinksComponent, pathMatch: 'full' },
   { path: 'to/:slug', component: ToComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToComponent } from './to/to.component';
+import { LinksComponent } from './links/links.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToComponent,
+    LinksComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
